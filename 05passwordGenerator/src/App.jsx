0724fd627c05a7e1b2cd340bc34eff67380983ae
Dipendra Chaudhary <ddipendrac@gmail.com>
2025-01-29 22:20:40 +0545

    currency converter

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
-
 function App() {
+
+  
+
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false) 
   const [charAllowed, setCharAllowed] = useState(false)
